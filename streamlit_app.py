96b304eea5059fae5bba6e573b9af097f845a5a0
notasgerais <164552573+notasgerais@users.noreply.github.com>
2024-09-25 14:59:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,6 +72,7 @@ st.header("Adicionar Questão")
 # in a form, the app will only rerun once the submit button is pressed.
 with st.form("add_ticket_form"):
     issue = st.text_area("Describe the issue")
+    Gabarito =  st.selectbox(["A","B","C", "D", "E", "Aberta"]),
     priority = st.selectbox("Priority", ["Fácil", "Média", "Difícil"])
     submitted = st.form_submit_button("Submit")
 
@@ -85,7 +86,7 @@ if submitted:
             {
                 "ID": f"Q{recent_ticket_number+1}",
                 "Conteúdo": issue,
-                "Gabarito": "Open",
+                "Gabarito": Gabarito,
                 "Dificuldade": priority,
                 "Date Submitted": today,
             }
